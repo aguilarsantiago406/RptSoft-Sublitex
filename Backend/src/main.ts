e54df772db57707ff66945863d60eac15d08617b
Santiago <aguilarfloressantiago406@gmail.com>
2026-09-21 11:57:51 -0500

    refactor(backend): ajuste en main.ts

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaExceptionFilter } from './core/filters/prisma-exception.filter';
-
+//holaaaaaaaaaaaaaaaaaa prueba mil
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
